a1799d6908f6744ce35e2b59d03df2d14eb93091
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-03-03 22:06:08 +0530

    create block, run hash, update the block hash

--- src/block.rs
+++ src/block.rs
@@ -17,7 +17,7 @@ impl Debug for Block {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "Block[{}]: \n Block Hash: {} \n timestamp: {} \n payload: {} ",
+            "\nBlock[{}]: \nBlock Hash: {} \ntimestamp: {} \npayload: {} \n",
             &self.index,
             &hex::encode(&self.block_hash),
             &self.timestamp,
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use blockchainlib::{Block, Hashable};
 fn main() {
-    println!("Hello, world!");
+    let mut block = Block::create_new_block(0, 0, 0, String::from("Payload"), vec![0; 32]);
+    println!("{:?}", &block);
+    let hash = block.hash();
+    println!("{:?}", &hash);
+
+    block.block_hash = hash;
+
+    println!("{:?}", &block)
 }
