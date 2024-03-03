26e3b962a2f5bc048700ccbf990089c7d93311ec
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-03-03 20:47:35 +0530

    impl debug for your block, and format

--- src/block.rs
+++ src/block.rs
@@ -1,10 +1,25 @@
+use std::fmt::Debug;
+
 use super::BlockHash;
 
 pub struct Block {
-    pub index : u32,
-    pub nonce : u64,
-    pub timestampt : u128,
-    pub payload : String,
-    pub block_hash : BlockHash,
-    pub prev_block_hash : BlockHash
+    pub index: u32,
+    pub nonce: u64,
+    pub timestamp: u128,
+    pub payload: String,
+    pub block_hash: BlockHash,
+    pub prev_block_hash: BlockHash,
+}
+
+impl Debug for Block {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Block[{}]: \n Block Hash: {} \n timestamp: {} \n payload: {} ",
+            &self.index,
+            &hex::encode(&self.block_hash),
+            &self.timestamp,
+            &self.payload,
+        )
+    }
 }
